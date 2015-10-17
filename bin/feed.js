59009455a60231cabc5d3d1871f1f58bbc3a47dd
spartako82 <spartako.bts@gmail.com>
2015-10-17 09:31:09 +0200

    feed: remove not active witnesses

--- bin/feed.js
+++ bin/feed.js
@@ -27,36 +27,45 @@ var cmd = function(client,args,cb){
   if(args.length > 0){
     asset = args[0].toUpperCase();
   }
-  client.get_asset(asset,function(err,_a){
+  client.info(function(err,info){
     if(err){return cb(err);}
-    var prec = Math.pow(10,5-_a.precision);
-    client.get_bitasset_data(asset,function(err,a){
+    console.log(info);
+    client.get_asset(asset,function(err,_a){
       if(err){return cb(err);}
-      var feeds = a.feeds;
-      var now = new Date();
-      var feeds = async.map(feeds, function(accs,_cb){
-        client.get_account(accs[0],function(err,a){
+      var prec = Math.pow(10,5-_a.precision);
+      client.get_bitasset_data(asset,function(err,a){
+        if(err){return cb(err);}
+        var feeds = a.feeds;
+        var now = new Date();
+        feeds = _.filter(feeds,function(accs){
+          return _.find(info.active_witnesses,function(i){
+            return accs[0] == i;
+          });
+        });
+        feeds = async.map(feeds, function(accs,_cb){
+          client.get_account(accs[0],function(err,a){
+            if(err){return cb(err)};
+            console.log(accs);
+            var date = new Date(accs[1][0]);
+            var delta = moment(now).diff(moment(date),"minutes");
+            var core_exchange_rate = accs[1][1].core_exchange_rate;
+            var value = prec*core_exchange_rate.base.amount/core_exchange_rate.quote.amount;
+            var res = _str.sprintf("%.8f",value);
+            var resInv = _str.sprintf("%.2f",1/value);
+            var mssr = accs[1][1].maximum_short_squeeze_ratio;
+            _cb(false,{account:a.name, date:date, delta:delta, res:res, resInv:resInv, mssr:mssr});
+          });
+        },function(err, feeds){
           if(err){return cb(err)};
-          console.log(accs);
-          var date = new Date(accs[1][0]);
-          var delta = moment(now).diff(moment(date),"minutes");
-          var core_exchange_rate = accs[1][1].core_exchange_rate;
-          var value = prec*core_exchange_rate.base.amount/core_exchange_rate.quote.amount;
-          var res = _str.sprintf("%.8f",value);
-          var resInv = _str.sprintf("%.2f",1/value);
-          var mssr = accs[1][1].maximum_short_squeeze_ratio;
-          _cb(false,{account:a.name, date:date, delta:delta, res:res, resInv:resInv, mssr:mssr});
+          feeds = _.sortBy(feeds,function(f){
+            return -f.date;
+          });
+          var res = "FEED "+asset+":\n";
+          res = _.reduce(feeds,function(s,f){
+            return s+_str.sprintf("%s min: %-4d mssr: %d %s\n",f.resInv,f.delta,f.mssr,f.account);
+          },"");
+          cb(false,res);
         });
-      },function(err, feeds){
-        if(err){return cb(err)};
-        feeds = _.sortBy(feeds,function(f){
-          return -f.date;
-        });
-        var res = ""
-        res = _.reduce(feeds,function(s,f){
-          return s+_str.sprintf("%s min: %-4d mssr: %d %s\n",f.resInv,f.delta,f.mssr,f.account);
-        },"");
-        cb(false,res);
       });
     });
   });
